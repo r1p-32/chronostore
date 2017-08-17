b1212b67b2293349a3f9897c26d6c0737ef81b66
r1p-32 <roma.kozin@gmail.com>
2017-08-17 11:26:09 +0300

    account: orders, order-details, payment

---
+++ app/inner-account-view-order.php
@@ -0,0 +1,202 @@
+<?php include 'header.html'; ?>
+
+<div class="breadcrumbs">
+	<div class="container">
+		<ul>
+			<li><a href="/">Home</a><i>&#xE805</i></li>
+			<li><span>Shop by Brand</span></li>
+		</ul>
+	</div>
+</div>
+
+<h1 class="page-title">Account</h1>
+
+<div class="container">
+	<div class="inner-section">
+		<div class="row">
+			<div class="col-sm-4">
+				<aside class="account-sidebar">
+					<div class="account-title">Account:</div>
+					<ul class="account-mnu">
+						<li><a href="javascript:void(0)">Account Dashboard</a></li>
+						<li><a href="javascript:void(0)">Account Information</a></li>
+						<li><a href="javascript:void(0)">Address Book</a></li>
+						<li class="active"><a href="javascript:void(0)">My Orders</a></li>
+						<li><a href="javascript:void(0)">Billing Agreements</a></li>
+						<li><a href="javascript:void(0)">My Wishlist</a></li>
+						<li><a href="javascript:void(0)">Saved Payment Information</a></li>
+						<li><a href="javascript:void(0)">Logout</a></li>
+					</ul>
+					<div class="account-sidebar__block">
+						<div class="account-title account-title_16">Compare product:</div>
+						<ul class="product-small-list">
+							<li>
+								<a href="javascript:void(0)" class="product-small-item clearfix">
+									<div class="product-small-item__img">
+										<img src="img/_product_1.jpg" class="img-responsive" alt="">
+									</div>
+									<div class="product-small-item__text">
+										<div class="product-small-item__name">Rolex Daytona Rolex Daytona Rolex Daytona</div>
+										<div class="product-small-item__model">116515LNI</div>
+										<div class="product-small-item__descr">18K Everose Gold Automatic</div>
+										<div class="product-small-item__price">$21,999.00</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="javascript:void(0)" class="product-small-item clearfix">
+									<div class="product-small-item__img">
+										<img src="img/_product_1.jpg" class="img-responsive" alt="">
+									</div>
+									<div class="product-small-item__text">
+										<div class="product-small-item__name">Rolex Daytona Rolex Daytona Rolex Daytona</div>
+										<div class="product-small-item__model">116515LNI</div>
+										<div class="product-small-item__descr">18K Everose Gold Automatic</div>
+										<div class="product-small-item__price">$21,999.00</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="javascript:void(0)" class="product-small-item clearfix">
+									<div class="product-small-item__img">
+										<img src="img/_product_1.jpg" class="img-responsive" alt="">
+									</div>
+									<div class="product-small-item__text">
+										<div class="product-small-item__name">Rolex Daytona Rolex Daytona Rolex Daytona</div>
+										<div class="product-small-item__model">116515LNI</div>
+										<div class="product-small-item__descr">18K Everose Gold Automatic</div>
+										<div class="product-small-item__price">$21,999.00</div>
+									</div>
+								</a>
+							</li>
+						</ul>
+						<a href="javascript:void(0)" class="btn btn_light">Go to compare</a>
+					</div>
+					<div class="account-sidebar__block">
+						<div class="account-title account-title_16">My orders:</div>
+						<div class="last-ordered-descr">Last Ordered Items</div>
+						<ul class="product-small-list">
+							<li>
+								<a href="javascript:void(0)" class="product-small-item clearfix">
+									<div class="product-small-item__img">
+										<img src="img/_product_1.jpg" class="img-responsive" alt="">
+									</div>
+									<div class="product-small-item__text">
+										<div class="product-small-item__name">Rolex Daytona Rolex Daytona Rolex Daytona</div>
+										<div class="product-small-item__model">116515LNI</div>
+										<div class="product-small-item__descr">18K Everose Gold Automatic</div>
+										<div class="product-small-item__price">$21,999.00</div>
+									</div>
+								</a>
+							</li>
+						</ul>
+						<div class="last-ordered-links clearfix">
+							<a href="javascript:void(0)" class="last-ordered__all">View All</a>
+							<a href="javascript:void(0)" class="btn btn_accent">Add to cart</a>
+						</div>
+					</div>
+				</aside>
+			</div>
+			<div class="col-sm-8 col-md-7">
+				<div class="account-title">
+					ORDER #100016005 - <span class="accent">CLOSED</span>
+					<div class="view-order-title-right">
+						<a href="javascript:void(0)" class="view-order-title-right__reorder">Reorder</a>
+						<i>&#xE81D</i>
+						<a href="javascript:void(0)" class="view-order-title-right__print">Print Order</a>
+					</div>
+				</div>
+				<div class="view-order-date">Order Date: November 24, 2016</div>
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="account-addres-book__title"><i>&#xE81C</i> Billing Address</div>
+						<div class="account-inner">
+							<ul class="account-addres-book__list">
+								<li>Sergeii Vlasov</li>
+								<li>DevSoft</li>
+								<li>Address</li>
+								<li>Address</li>
+								<li>Kharkov, Kharkivs'ka Oblast', 61000</li>
+								<li>Ukraine</li>
+								<li>Telephone: +380606002904 </li>
+								<li>Fax: 606002904</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="account-addres-book__title"><i>&#xE81E</i> Billing Address</div>
+						<div class="account-inner">
+							<ul class="account-addres-book__list">
+								<li><span>Credit Card</span></li>
+								<li>Card Number(Last 4):</li>
+								<li><span>0000</span></li>
+								<li>Credit Card Type</li>
+								<li><span>Master Card</span></li>
+							</ul>
+						</div>
+						<div class="account-addres-book__title"><i>&#xE81F</i> Billing Address</div>
+						<div class="account-inner">
+							<ul class="account-addres-book__list">
+								<li><span>Shipping Carrier - Standard (3-4 days)</span></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="account-block">
+					<div class="account-title">Items Ordered:</div>
+					<div class="items-ordered-wrap">
+						<table class="items-ordered">
+							<tbody>
+								<tr class="items-ordered__head">
+									<td class="items-ordered__name">Product Name</td>
+									<td>SKU</td>
+									<td>Price</td>
+									<td>Qty</td>
+									<td>Subtotal</td>
+								</tr>
+								<tr>
+									<td>
+										<div>Rolex Daytona 116515LNI</div> 	
+										<div>18K Everose Gold Automa  Everose Gold Automa</div> 	
+									</td>
+									<td>3789661568679</td>
+									<td>$21,999.00</td>
+									<td class="tar">
+										<div>Ordered: <strong>1</strong></div> 	
+										<div>Refunded: <strong>1</strong></div> 	
+									</td>
+									<td>$21,999.00</td>
+								</tr>
+								<tr>
+									<td colspan="4" class="tar">Subtotal</td>
+									<td>$21,999.00</td>
+								</tr>
+								<tr>
+									<td colspan="4" class="tar">Shipping & Handling</td>
+									<td>$0.84</td>
+								</tr>
+								<tr>
+									<td colspan="4" class="tar">Tax</td>
+									<td>$0.04</td>
+								</tr>
+								<tr>
+									<td colspan="4" class="tar"><strong>Grand Total</strong></td>
+									<td><strong>$21,999.88</strong></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-4">
+						<a href="javacript:void(0)" class="btn btn_light">Back to my orders</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+
+
+<?php include 'footer.html'; ?>
